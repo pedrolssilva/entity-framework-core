5ada321098c7a111bbdb06db059f7779fcdc967d
Pedro Luis <pedrolssilva@hotmail.com>
2022-09-08 20:13:16 -0300

    Recover data from database

--- Alura.Loja/Alura.Loja.Testes.ConsoleApp/Compra.cs
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/Compra.cs
@@ -1,11 +1,16 @@
 ﻿namespace Alura.Loja.Testes.ConsoleApp
 {
-    internal class Compra
+    public class Compra
     {
         public int Id { get; set; }
         public double Quantidade { get; internal set; }
         public int ProdutoId { get; set; }
         public Produto Produto { get; internal set; }
         public double Preco { get; internal set; }
+
+        public override string ToString()
+        {
+            return $"Compra de {Quantidade} do produto {Produto.Nome}";
+        }
     }
 }
--- Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -15,6 +15,93 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
+            using (var context = new StoreContext())
+            {
+                var serviceProvider = context.GetInfrastructure();
+                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+                loggerFactory.AddProvider(SqlLoggerProvider.Create());
+
+                var customer = context
+                    .Clientes
+                    .Include(c => c.EnderecoDeEntrega)
+                    .FirstOrDefault();
+
+                Console.WriteLine($"Endereço de entrega: {customer.EnderecoDeEntrega.Logradouro}");
+
+                var product = context
+                    .Produtos
+                    .Where(p => p.Id == 1)
+                    .FirstOrDefault();
+
+                context.Entry(product)
+                    .Collection(p => p.Compras)
+                    .Query()
+                    .Where(c => c.Preco > 10)
+                    .Load();
+
+
+                Console.WriteLine($"Mostrando as compras do produto {product.Nome}");
+                foreach (var item in product.Compras)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+
+        private static void ShowProductsFromSale()
+        {
+            using (var context = new StoreContext())
+            {
+                var serviceProvider = context.GetInfrastructure();
+                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+                loggerFactory.AddProvider(SqlLoggerProvider.Create());
+
+                var sale = context
+                    .Promocoes
+                    .Include(p => p.Produtos)
+                    .ThenInclude(pp => pp.Produto)
+                    .FirstOrDefault();
+                Console.WriteLine("\nMostrando os produtos da promoção...");
+                foreach (var item in sale.Produtos)
+                {
+                    Console.WriteLine(item.Produto);
+                }
+            }
+        }
+
+        private static void IncludeNewSale()
+        {
+            using (var context = new StoreContext())
+            {
+                var serviceProvider = context.GetInfrastructure();
+                var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+                loggerFactory.AddProvider(SqlLoggerProvider.Create());
+
+                var sale = new Promocao();
+                sale.Descricao = "Queima Total Janeiro 2023";
+                sale.DataInicio = new DateTime(2023, 1, 1);
+                sale.DataInicio = new DateTime(2023, 1, 31);
+
+                var product = context
+                    .Produtos
+                    .Where(p => p.Categoria == "Bebidas")
+                    .ToList();
+
+                foreach (var item in product)
+                {
+                    sale.IncludeProduct(item);
+                }
+
+                context.Promocoes.Add(sale);
+
+                ShowEntries(context.ChangeTracker.Entries());
+
+                context.SaveChanges();
+            }
+        }
+
+        private static void OneToOne()
+        {
             var fulano = new Cliente();
             fulano.Nome = "Fulaninho de Tal";
             fulano.EnderecoDeEntrega = new Endereco()
@@ -30,8 +117,8 @@ namespace Alura.Loja.Testes.ConsoleApp
             {
                 var serviceProvider = context.GetInfrastructure();
                 var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
-                loggerFactory.AddProvider(SqlLoggerProvider.Create()); 
-                
+                loggerFactory.AddProvider(SqlLoggerProvider.Create());
+
                 context.Clientes.Add(fulano);
                 context.SaveChanges();
             }
